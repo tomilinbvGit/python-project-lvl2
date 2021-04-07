c088be41bae83578a7f8f3935fbdf28ccc8bef48
tomilinBV <qwertere97@mail.ru>
2021-04-07 23:03:54 +0300

    commit_5.3

--- .venv/lib/python3.8/site-packages/gendiff/scripts/path.py
+++
@@ -1,6 +0,0 @@
-import os.path
-
-
-def abs_path(path):
-    absolute_path = os.path.abspath(path)
-    return absolute_path
--- .venv/lib/python3.8/site-packages/gendiff/scripts/sort.py
+++
@@ -1,3 +0,0 @@
-def sort(old_k, new_k):
-    shared_k = sorted(list(set(old_k + new_k)))
-    return shared_k
